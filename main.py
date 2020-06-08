daa3e6cba7bf284b31f783ad936d89a624856def
Kunal Dutta <kdutta@berkeley.edu>
2020-06-08 13:31:39 -0700

    added flask

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 from camera import SecurityFeed
+from flask import Flask, render_template, Response
+import threading
 
 PROTOTXT = "models/MobileNetSSD_deploy.prototxt.txt" # path to prototxt
 MODEL = "models/MobileNetSSD_deploy.caffemodel" # path to model
@@ -17,6 +19,25 @@ def start():
         if security.detect():
             security.record()
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(security),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    start()
+    t = threading.Thread(target=start, args=())
+    t.daemon = True
+    t.start()
+    app.run(host='0.0.0.0', debug=False)
