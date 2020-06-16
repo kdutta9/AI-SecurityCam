ffed766ca625d86f666e3034baf4688f7a87bda2
Kunal Dutta <kdutta@berkeley.edu>
2020-06-15 19:19:23 -0700

    Enabled downloading of frames when object detected

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, Response
 from camera import Camera
+from pathlib import Path
 
 app = Flask(__name__)
 model = "models/haarcascade_fullbody.xml"
+savePath = "saves/"
+feed = Camera(savePath)
 
+def mkdir():
+    print("Initializing save directory...")
+    Path(savePath).mkdir(parents=True, exist_ok=True)
 
 @app.route('/')
 def index():
@@ -19,8 +25,9 @@ def gen(camera):
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(feed), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
+    mkdir()
     app.run(host='0.0.0.0', threaded=True)
--- camera.py
+++ camera.py
@@ -1,11 +1,12 @@
 from imutils.video.pivideostream import PiVideoStream
-import imutils
 import time
 import cv2
 
 class Camera:
-    def __init__(self):
+    def __init__(self, path):
         self.vs = PiVideoStream().start()
+        self.detections = 0
+        self.path = path
         print("Starting camera...")
         time.sleep(5)
 
@@ -18,11 +19,17 @@ class Camera:
         cascade = cv2.CascadeClassifier(model)
         gray = cv2.cvtColor(readFrame, cv2.COLOR_BGR2GRAY)
         detections = cascade.detectMultiScale(gray, 1.3, 4)
+        found = False
 
         for (x, y, w, h) in detections:
-            print("Object detected...")
+            print("Object detected.")
+            self.detections += 1
+            found = True
             cv2.rectangle(readFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+        if found:
+            cv2.imwrite(self.path + "cap{num}.jpg".format(num=self.detections), readFrame)
+            print("Image saved to device.")
 
         buf = cv2.imencode('.jpg', readFrame)[1].tostring()
         return buf
