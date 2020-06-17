502fea078993939ce20c1997a3351d45f042cea8
Kunal Dutta <kdutta@berkeley.edu>
2020-06-16 17:25:54 -0700

    fixed indentation errors

--- camera.py
+++ camera.py
@@ -8,11 +8,11 @@ class Camera:
         self.vs = PiVideoStream().start()
         self.detections = 0
 		
-		# Set path to saved images.
+        # Set path to saved images.
         self.path = path
 		
-		# Let camera warm up for 5 seconds.
-		print("Starting camera...")
+        # Let camera warm up for 5 seconds.
+        print("Starting camera...")
         time.sleep(5)
         self.fps = FPS().start()
 
@@ -23,31 +23,31 @@ class Camera:
     def get_frame(self):
         readFrame = self.vs.read()
 		
-		# Get FPS reading.
+        # Get FPS reading.
         self.fps.update()
         self.fps.stop()
 
-		# Return bytes of frame.
+        # Return bytes of frame.
         buf = cv2.imencode('.jpg', readFrame)[1].tostring()
         return buf, self.fps.fps()
 
     def detect(self, model):
         readFrame = self.vs.read()
 		
-		# Initialize object detection.
+        # Initialize object detection.
         cascade = cv2.CascadeClassifier(model)
         gray = cv2.cvtColor(readFrame, cv2.COLOR_BGR2GRAY)
         detections = cascade.detectMultiScale(gray, 1.3, 4)
         found = False
 
-		# Draw rectangle around detections.
+        # Draw rectangle around detections.
         for (x, y, w, h) in detections:
             print("Object detected.")
             self.detections += 1
             found = True
             cv2.rectangle(readFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-		# Save image of detection to device.
+        # Save image of detection to device.
         if found:
             cv2.imwrite(self.path + "cap{num}.jpg".format(num=self.detections), readFrame)
             print("Image saved to device.")
